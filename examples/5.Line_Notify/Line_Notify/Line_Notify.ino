e3578c6bd86ea28fccec4a004f36c42eda995b59
Tantatorn Suksangwarn <tantatorn.s@kmi.tl>
2019-05-25 14:23:43 +0700

    Update Line_Notify.ino

--- examples/5.Line_Notify/Line_Notify/Line_Notify.ino
+++ examples/5.Line_Notify/Line_Notify/Line_Notify.ino
@@ -1,11 +1,11 @@
 #include <ESP8266WiFi.h>
 #include <CESmartCamp.h>
 
-const String TOKEN = "vOhcPRrN8mnO7urMx5vxLy2NyZEb8NwyIQy2hYacBDb";
+const String TOKEN = "Your Token";
 
 void setup() {
   Serial.begin(9600);
-  WiFi.begin("CE-ESL", "ceeslonly");
+  WiFi.begin("Your WIFI name", "Your WIFI password");
   Serial.println("connecting...");
   WiFi.mode(WIFI_STA);
   while (WiFi.status() != WL_CONNECTED) {
@@ -19,38 +19,6 @@ void setup() {
 }
 
 void loop() {
-  sendMessage(TOKEN, "Hello");
+  sendMessage(TOKEN, "Hello CESC");
   delay(5000);
-}
-
-void Line_Notify(String message) {
-  String msg = String("message=") + message;
-  axTLS::WiFiClientSecure client;
-  if (!client.connect("notify-api.line.me", 443)) {
-    Serial.println("connection failed");
-    return;
-  }
-  String req = "";
-  req += "POST /api/notify HTTP/1.1\r\n";
-  req += "Host: notify-api.line.me\r\n";
-  req += "Content-Type: application/x-www-form-urlencoded\r\n";
-  req += "Authorization: Bearer " + String(TOKEN) + "\r\n";
-  req += "Content-Length: " + String(msg.length()) + "\r\n";
-  req += "\r\n";
-  req +=  msg;
-  client.print(req);
-  unsigned long timeout = millis();
-  while (client.available() == 0) {
-    if (millis() - timeout > 5000) {
-      Serial.println(">>> Client Timeout !");
-      client.stop();
-      return;
-    }
-  }
-  while (client.available()) {
-    String line = client.readStringUntil('\r');
-    Serial.print(line);
-  }
-  Serial.println();
-  Serial.println("closing connection");
 }
